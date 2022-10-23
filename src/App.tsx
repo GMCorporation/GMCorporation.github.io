4eefdc39bb0c23e8ba3e54fc958e5e92515a48b5
D4N1L0-RED <danilodantas7002@gmail.com>
2022-10-22 21:44:26 -0300

    Hello World!
    Basic setup for React / Typescript / Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
